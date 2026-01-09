57de1c3f550577f7a083e4d077d15ff1ab4408cd
Ivan Marianek <ivan.marianek@gmail.com>
2026-01-09 17:02:03 +0100

    dbview getopt and printing

---
+++ src/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+#include "common.h"
+#include "file.h"
+#include "parse.h"
+
+void print_usage(char *argv[]) {
+    printf("Usage: %s =n -f <filepath>\n", argv[0]);
+    printf("\t -n - create new database file\n");
+    printf("\t -f - (required) path to database file\n");
+    return;
+}
+
+int main(int argc, char *argv[]) { 
+	bool newfile;
+    char *filepath = NULL;
+    int c;
+
+    while((c = getopt(argc, argv, "nf:")) != -1) {
+        switch (c) {
+            case 'n':
+                newfile = true;
+                break;
+            case 'f':
+                filepath = optarg;
+                break;
+            case '?':
+                printf("Unknown option %c\n", c);
+                break;
+            default:
+                return -1;
+        }
+    }
+
+    if (filepath == NULL) {
+        printf("Filepath is a required argument\n");
+        print_usage(argv);
+        return 0;
+    }
+    printf("Filepath: %s\n", filepath);
+    printf("Newfile: %d\n", newfile);
+    return 0;
+}
